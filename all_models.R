a7a984947a8383061757c7690adfdc2f33055a1c
Hassan Maatouk <maatouk_hassan@hotmail.fr>
2024-03-30 16:42:07 +0100

    Update all_models.R

--- all_models.R
+++ all_models.R
@@ -2,17 +2,15 @@
 ##### Function for MCMC samples using LS KLE algo & MUR #############
 #####################################################################
 
-
 source('all-fcts.R')
-# require(Rcpp)
+
 
 
 
 ### Function for drawing posterior samples using LS and MUR without hyperparameter updates (nu & ell):
-
-########### Nonparametric functions estimation ##############
-LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.in,
-                             tau.fix,sig.fix,xi.fix,sseed,verbose,return.plot,tol){
+########### Nonparametric functions estimation 
+LS.KLE.MUR.function <- function(y, x, N1, p, M, mcmc, brn, thin, nu, l, sig.in, xi.in, tau.in,
+                             tau.fix, sig.fix, xi.fix, sseed, verbose, return.plot, tol){
   # y:Response variable; x: vector to form design matrix \Psi (n X N+1)
   # N1: the number of knots of 1st subdomain
   # mcmc, brn, thin : mcmc samples, burning and thinning for MCMC
@@ -29,9 +27,9 @@ LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.i
   N <- M*N1
   y <- y[order(x)]
   x <- sort(x)
-  delta <- (max(x)-min(x))/(N-1)
-  my_knots <- seq(min(x),max(x),by=delta)
-  X <- fctv(x,my_knots,M,N1)
+  delta <- (max(x) - min(x)) / (N - 1)
+  my_knots <- seq(min(x), max(x), by = delta)
+  X <- fctv(x, my_knots, M, N1)
   
   if (missing(return.plot))
     return.plot <- TRUE
@@ -48,10 +46,10 @@ LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.i
     tol <- 1e-8
   
   if (missing(l))
-    l <- l_est(nu,c(my_knots[1],my_knots[length(my_knots)]),0.05)
+    l <- l_est(nu, c(my_knots[1], my_knots[length(my_knots)]), 0.05)
   
   # prior covariance K:
-  K <- kMat(my_knots,nu,l)
+  K <- kMat(my_knots, nu, l)
   # prior precision:
   K_inv <- tinv(K)
   
@@ -64,8 +62,8 @@ LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.i
   if (!missing(tau.fix)){
     tau.in <- tau.fix
     ## Hassan added two lines
-    Gamma <- tau.in*K
-    GX <- Gamma%*%t(X)
+    Gamma <- tau.in * K
+    GX <- Gamma %*% t(X)
   }
   
   if (!missing(sig.fix))
@@ -79,18 +77,18 @@ LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.i
     sig.in <- 1
   
   if (missing(xi.fix) && missing(xi.in))
-    xi.in <- mvrnorm(1,rep(0,N),K)
+    xi.in <- mvrnorm(1, rep(0,N), K)
   
   tau <- tau.in
   sig <- sig.in
   xi_in <- xi.in
   
-  em <- mcmc+brn
-  ef <- mcmc/thin
-  xi_sam <- matrix(NA,N,ef)
-  tau_sam <- rep(NA,ef)
-  sig_sam <- rep(NA,ef)
-  fhat_sam <- matrix(NA,n,ef)
+  em <- mcmc + brn
+  ef <- mcmc / thin
+  xi_sam <- matrix(NA, nrow = N, ncol = ef)
+  tau_sam <- rep(NA, ef)
+  sig_sam <- rep(NA, ef)
+  fhat_sam <- matrix(NA, nrow = n, ncol = ef)
   
   if (verbose)
     print("MCMC sample draws:")
@@ -100,18 +98,18 @@ LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.i
     # sampling Xi:
     y_tilde <- y 
     if (missing(xi.fix)){
-      fprior <- LS.KLE(my_knots,N1,p,M,nu=nu,l=l,tau=tau,tol=tol,sseedLS=i)
-      # as.vector(samp.WC(my_knots,nu_out,l_out,tau))
+      fprior <- LS.KLE(my_knots, N1, p, M, nu = nu, l = l, tau = tau, tol = tol, sseedLS = i)
+      # as.vector(samp.WC(my_knots, nu_out, l_out, tau))
       if (missing(tau.fix)){
-        Gamma <- tau*K
-        GX <- Gamma%*%t(X)
-        invXGX <- chol2inv(chol(X%*%GX+sig*diag(n)))
-        xi_out <- fprior+GX%*%invXGX%*%(y-X%*%fprior)
+        Gamma <- tau * K
+        GX <- Gamma %*% t(X)
+        invXGX <- chol2inv(chol(X %*% GX + sig * diag(n)))
+        xi_out <- fprior + GX %*% invXGX %*% (y - X %*% fprior)
       }
       else {
-        invXGX <- chol2inv(chol(X%*%GX+sig*diag(n)))
-        xi_out <- fprior+GX%*%invXGX%*%(y-X%*%fprior)
-        # xi_out <- LS.KLE_MUR(my_knots,f=fprior,A=X,y,N1,p,M,nu,l,tausq=tau,sqrt(sig))
+        invXGX <- chol2inv(chol(X %*% GX + sig * diag(n)))
+        xi_out <- fprior + GX %*% invXGX %*% (y - X %*% fprior)
+        # xi_out <- LS.KLE_MUR(my_knots, f = fprior, A = X, y, N1, p, M, nu, l, tausq = tau, sqrt(sig))
       }
     } else {
       xi_out <- xi_in
@@ -122,18 +120,18 @@ LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.i
     set.seed(123)
     # sampling \sigma^2:
     if (missing(sig.fix))
-      sig <- 1/rgamma(1,shape = n/2,rate = sum(y_star^2)/2)
+      sig <- 1 / rgamma(1, shape = n/2, rate = sum(y_star^2) / 2)
     
     # sampling \tau^2:
     if (missing(tau.fix))
-      tau <- 1/rgamma(1, shape = N/2, rate = (t(xi_out)%*%K_inv%*%xi_out)/2)
+      tau <- 1 / rgamma(1, shape = N/2, rate = (t(xi_out) %*% K_inv %*% xi_out) / 2)
     
     # storing MCMC samples:
     if (i > brn && i%%thin == 0){
-      xi_sam[,(i-brn)/thin] <- xi_out
-      sig_sam[(i-brn)/thin] <- sig
-      tau_sam[(i-brn)/thin] <- tau
-      fhat_sam[,(i-brn)/thin] <- Xxi
+      xi_sam[, (i - brn) / thin] <- xi_out
+      sig_sam[(i - brn) / thin] <- sig
+      tau_sam[(i - brn) / thin] <- tau
+      fhat_sam[, (i - brn) / thin] <- Xxi
     }
     
     if (i%%1000 == 0 && verbose){
@@ -143,28 +141,28 @@ LS.KLE.MUR.function <- function(y,x,N1,p,M,mcmc,brn,thin,nu,l,sig.in,xi.in,tau.i
     # renewing the initial value:
     xi_in <- xi_out
   } 
-  tm <- proc.time()-ptm
+  tm <- proc.time() - ptm
   
   
   fmean <- rowMeans(fhat_sam)
-  qnt <- apply(fhat_sam,1,function(x) quantile(x,c(0.025,0.975),na.rm = TRUE))
-  f_low <- qnt[1,]
-  f_upp <- qnt[2,]
-  ub <- max(f_low,f_upp,fmean,y)
-  lb <- min(f_low,f_upp,fmean,y)
+  qnt <- apply(fhat_sam, 1, function(x) quantile(x, c(0.025, 0.975), na.rm = TRUE))
+  f_low <- qnt[1, ]
+  f_upp <- qnt[2, ]
+  ub <- max(f_low, f_upp, fmean, y)
+  lb <- min(f_low, f_upp, fmean, y)
   
   if (return.plot){
-    par(mfrow=c(1,1))
-    par(mar=c(2.1,2.1,2.1,1.1)) # adapt margins
-    plot(x,y,pch='*',lwd=2,lty=1,col='black',
-         ylim=range(ub,lb),xlab='',ylab='')
-    polygon(c(x,rev(x)),y=c(f_low, rev(f_upp)),border=F,col='gray')
-    lines(x,fmean,type='l',lty=2,lwd=2)
-    points(x,y,pch='*')
+    par(mfrow = c(1, 1))
+    par(mar = c(2.1, 2.1, 2.1, 1.1)) # adapt margins
+    plot(x, y, pch = '*', lwd = 2, lty = 1, col = 'black',
+         ylim = range(ub, lb), xlab = '', ylab = '')
+    polygon(c(x, rev(x)), y = c(f_low, rev(f_upp)), border = F, col = 'gray')
+    lines(x, fmean, type = 'l', lty = 2, lwd = 2)
+    points(x, y, pch = '*')
   }
   
-  return(list("time"=tm,"xi_sam"=xi_sam,"sig_sam"=sig_sam,"tau_sam"=tau_sam,
-              "fhat_sam"=fhat_sam,"fmean"=fmean,"f_low"=f_low,"f_upp"=f_upp))
+  return(list("time" = tm, "xi_sam" = xi_sam, "sig_sam" = sig_sam, "tau_sam" = tau_sam,
+              "fhat_sam" = fhat_sam, "fmean" = fmean, "f_low" = f_low, "f_upp" = f_upp))
 }
 
                
